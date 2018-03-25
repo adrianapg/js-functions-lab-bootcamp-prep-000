2e526de0313aa67f0fa6c17cfd14f5e580487c2a
Adriana Perez <adriana.perez.garcia@outlook.com>
2018-03-25 22:47:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ return "Happy holidays!"
 }
 
 function happyHolidaysTo(name){
-  return  "Happy Holidays,"+name+"!"
+  return  "Happy Holidays," +name+"!"
 }
 /*
 function happyHolidayTo(holiday, name){
